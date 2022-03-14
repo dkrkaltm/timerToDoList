5d9926209392fafe0adfe6c8ce3d07c4b209b0cf
dkrkaltm <88367623+dkrkaltm@users.noreply.github.com>
2022-03-14 16:55:59 +0900

    cWrite edit

--- js/main.js
+++ js/main.js
@@ -5,12 +5,22 @@ var tWrite = document.querySelector("#timer span");
 
 //cWriteSelector
 var cSelects = document.querySelectorAll(".cSelects select");
-console.log(cSelects[0]);
 
+//cInfoSubmitSelector
+var cInfoSubmit = document.querySelector("#cInfoSubmit");
+
+
+//onCheck
+var onCheck  = document.querySelector("#onCheck");
+
+//cList
+var todayList = document.querySelector("#todayList");
+var morningList = document.querySelector("#morningList");
+var afternoonList = document.querySelector("#afternoonList");
 
 //variable
 var time ={
-    hour:1,
+    hour:"",
     minute:50,
     second:10,
     result:function(){
@@ -24,26 +34,52 @@ var time ={
 
 
 
+//cWrite select
+//cSelects
+optionCreate(1,100);
+optionCreate(2, 60);
+optionCreate(3, 60);
+
+function optionCreate(index,n){
+    for(let i = 0;i<n; i++){
+        let option = document.createElement('option');
+        option.innerText = i;
+        cSelects[index].append(option);        
+    }
+}
+function listCreate(){
+
+    let ul = (document.createElement('ul')).appendChild('li');
+    console.log(ul);
+
+}
+//cInfoSubmit click
+//c = check
+cInfoSubmit.addEventListener('click',function(event){
+    
+    var c = onCheck.checked;
+    var cCategory=cSelects[0].options[cSelects[0].options.selectedIndex].innerText;
+   time.hour = cSelects[1].options[cSelects[1].options.selectedIndex].innerText;
+   listCreate();
+});
+
+
 // timer function
 // 선택한 todo에 따른 시간
 // 총합 초를 감소 시키며 시 분 초 구하기
 var tResult = time.result();
 
 var timer =setInterval(function(){
-   
     let time ={
         hour: Math.floor(tResult/3600),
         minute: Math.floor((tResult%3600)/60),
         second: Math.floor((tResult%3600)%60)
     };
    digit(time);
-
-   tWrite.textContent = time.hour+":"+time.minute+":"+time.second;
+   tWrite.innerText = time.hour+":"+time.minute+":"+time.second;
    console.log("시:"+time.hour+"분:"+time.minute+"초"+time.second);
    tResult--;
-
 },1000)
-
 // 자릿수에 따라 0채우기
 function digit(val){
   
@@ -58,9 +94,3 @@ function digit(val){
     }
     
 }
-
-//cWrite select
-//cSelects
-cSelects[0].append("<option value=''+"+'오늘'+'+>'+"</option>");
-cSelects[0].appendChild("오전");
-cSelects[0].appendChild("오후");
