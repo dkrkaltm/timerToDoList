0f7f49b044e352026fdd4f83f1832e2c60a2bcf2
dkrkaltm <88367623+dkrkaltm@users.noreply.github.com>
2022-03-15 16:47:41 +0900

    cWrite Edit 2

--- js/main.js
+++ js/main.js
@@ -6,29 +6,39 @@ var tWrite = document.querySelector("#timer span");
 //cWriteSelector
 var cSelects = document.querySelectorAll(".cSelects select");
 
-//cInfoSubmitSelector
-var cInfoSubmit = document.querySelector("#cInfoSubmit");
+//cInfoSubmit
+var cInfoSubmit = document.querySelector("#cWriteForm");
 
 
 //onCheck
 var onCheck  = document.querySelector("#onCheck");
 
 //cList
-var todayList = document.querySelector("#todayList");
-var morningList = document.querySelector("#morningList");
-var afternoonList = document.querySelector("#afternoonList");
+var List = document.querySelectorAll("#cList section");
+//cInfo
+var cInfo = document.querySelectorAll(".cInfo input");
+var cMemo = document.querySelector("#cMemo");
 
-//variable
-var time ={
+
+
+//object variable
+var cWriteImf={
+    onCheck:"",
+    cCategory:"",
+    cTitle:"",
+    cTarget:"",
+    cMemo:"",
+}
+var time={
     hour:"",
-    minute:50,
-    second:10,
+    minute:"",
+    second:"",
     result:function(){
         this.hour= this.hour*3600;
         this.minute= this.minute*60;
-        
         return this.hour+this.minute+this.second;
     }
+   
 };
 
 
@@ -43,43 +53,58 @@ optionCreate(3, 60);
 function optionCreate(index,n){
     for(let i = 0;i<n; i++){
         let option = document.createElement('option');
-        option.innerText = i;
-        cSelects[index].append(option);        
+        option.innerText = i; 
+        cSelects[index].append(option);       
     }
 }
-function listCreate(){
 
-    let ul = (document.createElement('ul')).appendChild('li');
-    console.log(ul);
+function listCreate(){
+    digit(time);
+
+  
+    if(cWriteImf.cCategory =="오늘"){
+        List[0].innerHTML="<ul><li><div><span>"+cWriteImf.cCategory+"</span><span>"+time.hour+":"+time.minute+":"+time.second+"</span></div><h3>"+cWriteImf.cTitle+"</h3></li></ul>" ;
+    }else if(cWriteImf.cCategory =="오전"){
+
+    }else{
+
+    }
+    
+}
+function write(event){
+event.preventDefault();
+cWriteImf.onCheck = onCheck.checked; // 반복 여부
+cWriteImf.cCategory=cSelects[0].options[cSelects[0].selectedIndex].innerText; // 카테고리 값
+cWriteImf.cTitle =cInfo[0].value;
+cWriteImf.cTarget = cInfo[1].value;
+cWriteImf.cMemo= cMemo.value;
+
+time.hour= cSelects[1].options[cSelects[1].selectedIndex].innerText; //시간 값
+time.minute = cSelects[2].options[cSelects[2].selectedIndex].value;
+time.second = cSelects[3].options[cSelects[3].selectedIndex].innerText;
+listCreate();
 
 }
 //cInfoSubmit click
 //c = check
-cInfoSubmit.addEventListener('click',function(event){
-    
-    var c = onCheck.checked;
-    var cCategory=cSelects[0].options[cSelects[0].options.selectedIndex].innerText;
-   time.hour = cSelects[1].options[cSelects[1].options.selectedIndex].innerText;
-   listCreate();
-});
-
+cInfoSubmit.addEventListener('submit',write);
 
 // timer function
 // 선택한 todo에 따른 시간
 // 총합 초를 감소 시키며 시 분 초 구하기
-var tResult = time.result();
+var tResult =time.result();
 
-var timer =setInterval(function(){
-    let time ={
-        hour: Math.floor(tResult/3600),
-        minute: Math.floor((tResult%3600)/60),
-        second: Math.floor((tResult%3600)%60)
-    };
-   digit(time);
-   tWrite.innerText = time.hour+":"+time.minute+":"+time.second;
-   console.log("시:"+time.hour+"분:"+time.minute+"초"+time.second);
-   tResult--;
-},1000)
+// var timer =setInterval(function(){
+//     let time ={
+//         hour: Math.floor(tResult/3600),
+//         minute: Math.floor((tResult%3600)/60),
+//         second: Math.floor((tResult%3600)%60)
+//     };
+//    digit(time);
+//    tWrite.innerText = time.hour+":"+time.minute+":"+time.second;
+//    console.log("시:"+time.hour+"분:"+time.minute+"초"+time.second);
+//    tResult--;
+// },1000)
 // 자릿수에 따라 0채우기
 function digit(val){
   
